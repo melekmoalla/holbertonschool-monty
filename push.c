e03ff098bfe6326ae8600123c5406daeeebd8677
melekmoalla <melekmoalla2468@gmail.com>
2022-12-21 14:43:19 +0100

    test z

--- push.c
+++ push.c
@@ -2,13 +2,17 @@
 
 void p_push(stack_t **stack, unsigned int counter)
 {
-    int n, j = 0;
+    int n, j = 0, z;
 
     if (val)
     {
         for (j = 0; val[j] != '\0'; j++)
         {
             if (val[j] > '9' || val[j] < '0')
+            {
+                z = 1;
+            }
+            if (z == 1)
             {
                 fprintf(stderr, "L%d: usage: push integer\n", counter);
                 free_stack(*stack);
