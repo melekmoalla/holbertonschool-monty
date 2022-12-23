05289a1000e67b795db9602fb0a6e060c3800dbe
melekmoalla <melekmoalla2468@gmail.com>
2022-12-23 09:20:59 +0100

    add condition of 2

--- rotr.c
+++ rotr.c
@@ -2,13 +2,24 @@
 
 void p_rotr(stack_t **stack, __attribute__((unused)) unsigned int count)
 {
+    int i = 0;
+
     if (*stack == NULL)
     {
         return;
     }
 
-    stack_t *last = *stack;
-
+    stack_t *last = *stack, *h = *stack;
+    
+    while (h)
+    {
+        h = h->next;
+        i++;
+    }
+    if (i < 2)
+    {
+        return;
+    }
     while (last->next != NULL)
     {
         last = last->next;
